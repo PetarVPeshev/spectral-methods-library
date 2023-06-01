50430b9dd18e383e6c69ade4aa37b3a634553e4a
Petar V Peshev <40143832+PetarVPeshev@users.noreply.github.com>
2023-06-01 20:07:30 +0200

    added functions for superstrate dispersion and krho calculation

--- dispersion_eqn.m
+++ dispersion_eqn.m
@@ -27,6 +27,36 @@ function [D_te, D_tm] = dispersion_eqn(k0, krho, varargin)
         % Denominator
         D_te = Zair_te + Zin_te;
         D_tm = Zair_tm + Zin_tm;
+    elseif strcmp(varargin{1}, 'Superstrate')
+        air_length = varargin{2};
+        slab_length = varargin{3};
+        slab_er = varargin{4};
+        
+        slab_k = k0 * sqrt(slab_er);
+        slab_impedance = wave_impedance / sqrt(slab_er);
+        
+        air_kz = - 1j * sqrt( - k0 ^ 2 + krho .^ 2 );
+        slab_kz = - 1j * sqrt( - slab_k ^ 2 + krho .^ 2 );
+    
+        % Air impedance
+        Zair_te = wave_impedance * k0 ./ air_kz;
+        Zair_tm = wave_impedance * air_kz / k0;
+    
+        % Substrate impedance
+        Zs_te = slab_impedance * slab_k ./ slab_kz;
+        Zs_tm = slab_impedance * slab_kz / slab_k;
+    
+        % Input impedance seen at interface 1
+        Zl_te = Zs_te ...
+            .* (Zair_te + 1j * Zs_te .* tan(slab_k * slab_length)) ...
+            ./ (Zs_te + 1j * Zair_te .* tan(slab_k * slab_length));
+        Zl_tm = Zs_tm ...
+            .* (Zair_tm + 1j * Zs_tm .* tan(slab_k * slab_length)) ...
+            ./ (Zs_tm + 1j * Zair_tm .* tan(slab_k * slab_length));
+    
+        % Denominator
+        D_te = Zl_te + 1j * Zair_te .* tan(- air_kz * air_length);
+        D_tm = Zl_tm + 1j * Zair_tm .* tan(- air_kz * air_length);
     end
 end
 
